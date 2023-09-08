7f8715bdfed22edf76662553085b571d2bad080c
tommasomercuri <tommasomercuri2004@gmail.com>
2023-09-08 10:39:56 +0200

    Fixed UUID Parameter inside createNewUser() Service

--- src/main/java/autoWiredV1/services/impl/CreateNewUserServiceImpl.java
+++ src/main/java/autoWiredV1/services/impl/CreateNewUserServiceImpl.java
@@ -3,8 +3,10 @@ package autoWiredV1.services.impl;
 import autoWiredV1.model.DataRequest;
 import autoWiredV1.model.User;
 import autoWiredV1.services.CreateNewUserService;
+import autoWiredV1.services.CreateNewUuidService;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +15,13 @@ import org.springframework.stereotype.Service;
 @Data
 @NoArgsConstructor
 public class CreateNewUserServiceImpl implements CreateNewUserService {
+    @Autowired
+    CreateNewUuidService createNewUuidService;
     @Override
     public User createNewUser( String name, String bornYear) {
         System.out.println(name);
         System.out.println(bornYear);
-            User user = new User("", name, bornYear);
+            User user = new User(createNewUuidService.createNewUuid(), name, bornYear);
             System.out.println("Created new user:" + user.toString());
         return user;
     }
